419134bc10acc3b22d8f389c4866bbb6417ecb1c
elmabt <elmabristric20@gmail.com>
2022-01-03 14:19:54 +0100

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MoviesliderComponent } from './movieslider/movieslider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MoviesliderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/movieslider/movieslider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movieslider',
+  templateUrl: './movieslider.component.html',
+  styleUrls: ['./movieslider.component.css']
+})
+export class MoviesliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
