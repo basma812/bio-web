9f5d3159cd16a37050bb03e9412d9d3bebc8a86b
elmabt <elmabristric20@gmail.com>
2022-01-04 15:20:18 +0100

    slider working but wrong size

--- src/app/moviesliders/moviesliders.component.ts
+++ src/app/moviesliders/moviesliders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-moviesliders',
@@ -10,6 +11,32 @@ export class MovieslidersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  customOptions: OwlOptions = {
+      loop: true,
+      mouseDrag: false,
+      touchDrag: false,
+      pullDrag: false,
+      dots: false,
+      navSpeed: 700,
+      navText: ['', ''],
+      responsive: {
+        0: {
+          items: 1
+        },
+        400: {
+          items: 2
+        },
+        740: {
+          items: 3
+        },
+        940: {
+          items: 4
+        }
+      },
+      nav: true
+   
   }
 
 }
