891f3e4231e74530744b80b00b922fd126dcb36a
Basma Abdulahad <basma_812@hotmail.com>
2022-01-06 14:55:50 +0100

    cinema seats theater done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { SeatsComponent } from './seats/seats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    SeatsComponent
   ],
   imports: [
     BrowserModule,
