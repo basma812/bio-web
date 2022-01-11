40482f4f6e85bbce8b21270ee6ce72a9605f20b6
elmabt <elmabristric20@gmail.com>
2022-01-11 14:32:11 +0100

    some changes

--- src/app/date-page/date-page.component.ts
+++ src/app/date-page/date-page.component.ts
@@ -12,20 +12,22 @@ export class DatePageComponent implements OnInit {
 
   description: movied[] = [
     new movied(
+      1,
       'Drive',
       'this is simply to see if it works',
       'assets/img/drivep.jpg',
       '2021-12-16',
       'horror',
-      1,
+      
     ),
     new movied(
+      2,
       'pulp fiction',
       'this is simply to see if it works',
       'assets/img/pulp-fictionp.jpg',
       '2021-12-16',
       'horror',
-      2,
+      
     ),
   ];
 
--- src/app/date-page/movied.model.ts
+++ src/app/date-page/movied.model.ts
@@ -1,18 +1,20 @@
 export class movied {
+    public id: number;
     public name: string;
     public description: string;
     public imagePath: string;
     public productionDate :  string;
     public genre: string; 
-    public id: number;
     
-constructor(name: string, desc: string, imagePath: string, productionDate: string, genre : string, id : number,) {
+
+constructor(id : number, name: string, desc: string, imagePath: string, productionDate: string, genre : string, ) {
+    this.id = id;
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
     this.productionDate = productionDate;
     this.genre = genre;
-    this.id = id;
+    
 }
 
 }
