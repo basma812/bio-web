37d6bee39710d22a6424a4eb8959bb855a6b13ba
elmabt <elmabristric20@gmail.com>
2022-01-17 13:16:15 +0100

    routing?

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 import { DatePageComponent } from './date-page/date-page.component';
 import { HomeComponent } from './home/home.component';
+import { SeatsComponent } from './seats/seats.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'date-page', component: DatePageComponent},
+  { path: 'seats', component: SeatsComponent},
+  { path: 'cart', component: CartComponent }
  
 ];
 
