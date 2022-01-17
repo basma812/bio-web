e51c424e14ba3f45c585846f697915c8c86a85d5
Basma Abdulahad <basma_812@hotmail.com>
2022-01-17 11:26:40 +0100

    cart error fixed

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators} from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-import { SeatsComponent } from '../seats/seats.component';
-
-import { Observable } from "rxjs";
-import { tap } from "rxjs/operators";
 
 @Component({
   selector: 'app-cart',
@@ -13,7 +8,7 @@ import { tap } from "rxjs/operators";
 })
 export class CartComponent implements OnInit {
   SeatsComponent!: string;
-  items = this.seatsComponent.getItems();
+
   bookingForm!: FormGroup;
  
 
@@ -27,27 +22,12 @@ export class CartComponent implements OnInit {
     email: new FormControl('',[Validators.required,Validators.email]),
     phoneNumber: new FormControl('',[Validators.required,Validators.pattern('[0-9]'),Validators.maxLength(10)]),
   
-    shippingForm = this.formBuilder.group({
-      name: '',
-      address: ''
-    })
+
   })
 
   get name(){return this.shippingForm.get('name')}
   get email(){return this.shippingForm.get('email')}
   get phoneNumber(){return this.shippingForm.get('phoneNumber')}
 
- 
-  
-  constructor(
-    private seatsComponent: SeatsComponent,
-    private formBuilder: FormBuilder,
-  ) {}
 
-  onSubmit(): void {
-    // Process checkout data here
-    this.items = this.seatsComponent.clearCart();
-    console.warn('Your order has been submitted', this.shippingForm.value);
-    this.shippingForm.reset();
-  }
 }
