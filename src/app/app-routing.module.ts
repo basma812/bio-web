fd33dc681f4d94694c8f23f8a9a3577f8edfdaf4
elmabt <elmabristric20@gmail.com>
2022-01-17 11:10:36 +0100

    alot of stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DatePageComponent } from './date-page/date-page.component';
 import { HomeComponent } from './home/home.component';
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'date-page', component: DatePageComponent},
+  { path: '', component: }
 ];
 
 @NgModule({
